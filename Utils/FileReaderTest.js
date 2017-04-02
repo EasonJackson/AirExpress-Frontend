3e5de5f3e799e56c66e3fd36dd3ce5c6bb9e1a18
Eason <pyhanson@gmail.com>
2017-04-02 12:38:04 +0800

    New Airport file

--- Utils/FileReaderTest.js
+++ Utils/FileReaderTest.js
@@ -1,7 +1,9 @@
 var fs = require("fs");
 console.log("Starting");
-fs.readFile("test.txt", "utf8", function(error, data) {
+fs.readFile("Airports.json", function(error, data) {
     if(error) throw error;
     console.log("Contents of file: " + data);
+    result = JSON.parse(data);
+    console.log(result[0]);
 });
 console.log("Carrying on executing");
